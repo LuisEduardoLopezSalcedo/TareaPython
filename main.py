d25993ff66c129c7891fa492e5b7d7822dca54fc
Luis Eduardo <loistronics@gmail.com>
2022-05-17 23:42:35 -0430

    Tarea finalizada y corregida

--- main.py
+++ main.py
@@ -16,26 +16,21 @@ def lecturaArchivo(archivo):
             edad = filaOrganizada[2]
             profesion = filaOrganizada[3]
             
-            consulta = curSelect.execute(f"""
-                Select * from datosPersona where cedula = '{cedula}' 
-            """).fetchall()            
+            consulta = curSelect.execute(f"Select * from datosPersona where cedula = '{cedula}'").fetchall()    
             
             if len(consulta) >= 1: 
                 print("ACTUALIZANDO")
-                curUpdate.execute(f"""UPDATE datosPersona set nombre = '{nombre}', edad = '{edad}', profesion = '{profesion}' where cedula = '{cedula}' """) 
+                curUpdate.execute(f"UPDATE datosPersona set nombre = '{nombre}', edad = '{edad}', profesion = '{profesion}' where cedula = '{cedula}'") 
                 db.commit()
                 
             if len(consulta) == 0: 
                 print("INSERTANDO")
-                curInsert.execute(f"""INSERT into datosPersona(cedula,nombre,edad,profesion) VALUES ('{cedula}','{nombre}','{edad}','{profesion}')""")
+                curInsert.execute(f"INSERT into datosPersona(cedula,nombre,edad,profesion) VALUES ('{cedula}','{nombre}','{edad}','{profesion}')")
                 db.commit()
                 
             
            
 if __name__ == '__main__':
-
-    
-
     
     lecturaArchivo("datos.csv")
 
