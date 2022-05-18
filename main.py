8c0adca19cb3651e84e7fbfae740c4984a4808b8
Luis Eduardo <loistronics@gmail.com>
2022-05-17 23:29:58 -0430

    Tarea finalizada

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
+import csv
+import sqlite3
 
+def lecturaArchivo(archivo):
 
-if __name__ = '__main__':
+    db = sqlite3.connect("Persona.db")
+    curSelect = db.cursor()
+    curUpdate = db.cursor()
+    curInsert = db.cursor()
+    
+    with open("datos.csv") as documento:
+        for fila in documento:
+            filaOrganizada = fila.split(";")
+            cedula = filaOrganizada[0]
+            nombre = filaOrganizada[1]
+            edad = filaOrganizada[2]
+            profesion = filaOrganizada[3]
+            
+            consulta = curSelect.execute(f"""
+                Select * from datosPersona where cedula = '{cedula}' 
+            """).fetchall()            
+            
+            if len(consulta) >= 1: 
+                print("ACTUALIZANDO")
+                curUpdate.execute(f"""UPDATE datosPersona set nombre = '{nombre}', edad = '{edad}', profesion = '{profesion}' where cedula = '{cedula}' """) 
+                db.commit()
+                
+            if len(consulta) == 0: 
+                print("INSERTANDO")
+                curInsert.execute(f"""INSERT into datosPersona(cedula,nombre,edad,profesion) VALUES ('{cedula}','{nombre}','{edad}','{profesion}')""")
+                db.commit()
+                
+            
+           
+if __name__ == '__main__':
 
-    print("Empezamos")
+    
+
+    
+    lecturaArchivo("datos.csv")
+
+    
+
+    
+    
